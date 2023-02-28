7a5495ead26e40b8d66edf826e1ac290590d9ffa
alyssakkim <alyssakkim@naver.com>
2023-02-28 16:19:56 +0900

    css 작업

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 // 무비 컴포넌트
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie({movieId, coverImg, title, summary, genres}) {
         // props (Movie 컴포넌트는 위 속성들을 부모 컴포넌트로부터 받아옴)
     return (
-     <div>
+     <div className="{styles.movie}">
         <img src={coverImg} alt={title} />
         <h2>
             <Link to={`/movie/${movieId}`}>{title}</Link>
