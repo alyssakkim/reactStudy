edbfd89ae489e57310dc3699ee622a0951ddcf9e
alyssakkim <alyssakkim@naver.com>
2023-02-28 16:40:02 +0900

    edit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import App from "./App";
-import "./styles.css";
 
 // react 어플리케이션 렌더링
 
