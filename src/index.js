723da3546a0c57f16aca05a6087bb4ac7e89e6cb
alyssakkim <alyssakkim@naver.com>
2023-02-28 16:29:03 +0900

    index 에 styles.css import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import App from "./App";
+import "./styles.css";
 
 // react 어플리케이션 렌더링
 
